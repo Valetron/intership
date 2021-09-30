3fea377dfe1299f77136b315e51a884ceb0fc6b2
Veler Jevsky <43819843+Valetron@users.noreply.github.com>
2021-09-30 17:18:35 +0300

    Flask exporter fixed

---
+++ task_0/flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route('/')
+def main():
+    return "Hello, world"
+
+@app.route('/simple')
+def simple_get():
+    pass
+    
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
